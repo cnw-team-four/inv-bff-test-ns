9255251c0db1450dcc938b124028402f14f392ed
Noé Samaille <noe.samaille@ibm.com>
2022-08-24 21:02:19 +0000

    Default SERVICE_URL
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/config/stock-item-service.config.provider.ts
+++ src/config/stock-item-service.config.provider.ts
@@ -1,6 +1,6 @@
 import {ObjectFactory} from 'typescript-ioc';
 
-const baseUrl: string = process.env.SERVICE_URL || 'localhost:9080';
+const baseUrl: string = process.env.SERVICE_URL || 'http://inv-svc-test-ns:9080';
 
 export const stockItemConfigFactory: ObjectFactory = () => ({
   baseUrl,
