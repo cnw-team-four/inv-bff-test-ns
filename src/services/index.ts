3554479e902613f8e1da94f2223820b3f19495d5
Noé Samaille <noe.samaille@ibm.com>
2022-08-24 20:40:55 +0000

    Adds service implementation
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,6 +5,7 @@ export * from './project.api';
 export * from './task.api';
 export * from './stock-items.api';
 export * from './stock-items-mock.service';
+export * from './stock-items.service';
 
 import config from './ioc.config';
 
